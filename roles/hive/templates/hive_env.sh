58f7bc55ed655ea4c851649a1946b740a6b1601a
Armin EsmaeilZadeh <armin.esmaeilzadeh@gmail.com>
2020-10-19 15:26:44 -0700

    s

--- roles/hive/templates/hive_env.sh
+++ roles/hive/templates/hive_env.sh
@@ -1,6 +1,7 @@
+export HADOOP_HOME={{hadoop_install_path}}/hadoop-{{hadoop_version}}
+export PATH=$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH
+
 export HIVE_HOME={{hive_path}}/apache-hive-{{hive_version}}-bin
 export PATH=$HIVE_HOME/bin:$PATH
 export CLASSPATH=$CLASSPATH.:$HIVE_HOME/lib
 export HIVE_CONF_DIR={{ hive_path }}/apache-hive-{{ hive_version}}-bin/conf
-export HADOOP_HOME={{hadoop_install_path}}/hadoop-{{hadoop_version}}
-export PATH=$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH
